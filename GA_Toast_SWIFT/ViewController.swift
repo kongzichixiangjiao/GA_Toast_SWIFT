580f73a6c6fac7de625a374ce3be7a3b32017ac6
kongzichixiangjiao <58895347@qq.com>
2016-04-20 21:15:52 +0800

    Initial Commit

---
+++ GA_Toast_SWIFT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GA_Toast_SWIFT
+//
+//  Created by houjianan on 16/4/20.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
